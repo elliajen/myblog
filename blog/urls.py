3f21b3d9146fe78663a757d5eb32f9ef9458e134
elliajen <dmstndhk123@naver.com>
2021-10-22 13:24:28 +0900

    추가

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    #127.0.0.1:8000/blog
+    path('', views.index),
+    path('<int:post_id>/', views.detail, name='detail'),
+    path('post/create/', views.post_create, name='post_create'),
+]
---
+++ blog/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from blog.forms import PostForm
+from blog.models import Post
+
+
+def index(request):
+    post_list = Post.objects.all()
+    context = {'post_list': post_list}
+    return render(request, 'blog/index.html', context)
+    # return HttpResponse("안녕하세요~ 블로그 사이트입니다.")
+
+
+def detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context = {'post': post}
+    return render(request, 'blog/detail.html', context)
+
+
+# 포스트 등록
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:index')
+    else:
+        form = PostForm()
+    context = {'form': form}
+    return render(request, 'blog/post_form.html', context)
